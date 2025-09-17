3fe0b81efedc0d02b86eb9155361a1e3552cf1c2
joao_junio <joaojunio818@gmail.com>
2025-09-17 15:44:29 -0300

    Modificando warning

--- src/services/language_service.js
+++ src/services/language_service.js
@@ -8,7 +8,12 @@ const i18n = {
 		name: "Name",
 		login: "Login",
 		haveAccount: "I have an acoount <a href='login.html'>Login</a",
-		titleMessageError: "Sorry!"
+		titleMessageError: "Sorry!",
+		h2Warning: "Choose a strong password:",
+		sixCaracter: "- 6 characters",
+		oneCapitalLetter: "- 1 capital letter",
+		oneNumber: "- 1 number",
+		oneSpecialCaracter: "- 1 special character",
 	},
 	pt: {
 		theme: "Tema",
@@ -19,7 +24,12 @@ const i18n = {
 		name: "Nome",
 		login: "Entrar",
 		haveAccount: "Já tenho uma conta <a href='login.html'>Entrar</a",
-		titleMessageError: "Desculpe!"
+		titleMessageError: "Desculpe!",
+		h2Warning: "Escolha uma senha forte:",
+		sixCaracter: "- 6 caracteres",
+		oneCapitalLetter: "- 1 letra maiúscula",
+		oneNumber: "- 1 número",
+		oneSpecialCaracter: "- 1 caracter especial",
 	}
 };
 
